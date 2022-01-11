56b7918afd7fa9c2dced3b1fb2a9b2ac4defb586
MrNedNick <mrnediala@gmail.com>
2022-01-11 16:05:40 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div id="app">
     <Header></Header>
-    <Input></Input>
-    <List></List>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import Input from './components/Input.vue'
-import List from './components/List.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Input,
-    List,
+
   }
 }
 </script>
 
-<style scoped>
-  body {
-  font-family: 'Ubuntu', sans-serif;
-  font-weight: 300;
-}
-  
-</style>
+
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,29 @@
 <template>
-    <div> 
+    <div class="main"> 
         <h2>Vue Todo App</h2>
 
         <!-- input -->
-        <div>
-            <input @keyup.enter="submitTask" v-model="task" type="text" placeholder="Enter task">
-            <button @click="submitTask" >SUBMIT</button>
+        <div >
+            <input class="input" @keyup.enter="submitTask" v-model="task" type="text" placeholder="Enter task">
+            <button class="submit" @click="submitTask" >SUBMIT</button>
         </div>
 
-        <!-- TABLE -->
-        <table>
-            <thead>
-                <tr>
-                    <th>Task</th>
-                    <th>#</th>
-                    <th>#</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(task, index) in tasks" :key='index'>
-                    <th>{{ task.name }}</th>
-                    <th>
-                        <div @click="editTask(index)">
-                            <span class="fa fa-pen"></span>
-                        </div>
-                    </th>
-                    <th>
-                        <div @click="deleteTask(index)">
-                            <span class="fa fa-trash"></span>
-                        </div>
-                    </th>
-                </tr>
-            </tbody>
-            
-        </table>
-
+        <!-- LIST -->
+        <div class="list" v-for="(task, index) in tasks" :key='index'>
+            <div class="list__item">
+                <div class='item__name'>{{ task.name }}</div>
+                <div class="item__edit">
+                    <div class="edit__item" @click="editTask(index)">
+                        <span class="fa fa-pen"></span>
+                    </div>
+                    <input class="edit__item" type="checkbox">
+                    <div class="edit__item" @click="deleteTask(index)">
+                        <span class="fa fa-trash"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </template>
 
@@ -87,4 +76,38 @@ export default {
 
 
 </script>
+
+<style scoped>
+.main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.input{
+    height: 24px;
+    width: 300px;
+}
+.submit {
+    width: 100px;
+    height: 30px;
+    border-color: rgb(238, 223, 196);
+    background-color: rgb(230, 236, 177);
+    font-weight: 700;
+}
+.list__item {
+    display: flex;
+    justify-content: space-between;
+    width: 400px;
+    margin: 10px;
+}
+.item__name {
+    
+}
+.item__edit {
+    display: flex;
+}
+.edit__item{
+    margin-right: 10px;
+}
+</style>
 
